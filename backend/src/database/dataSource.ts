f1832e1aa34159001f3356df40eec862704efc22
renato3x <pereirarenato21@gmail.com>
2022-11-11 01:11:02 -0300

    backend: using dotenv

--- backend/src/database/dataSource.ts
+++ backend/src/database/dataSource.ts
@@ -6,12 +6,12 @@ import { Kinship } from './entities/Kinship'
 import { User } from './entities/User'
 
 export const dataSource: DataSource = new DataSource({
-  type: "mssql",
-  host: "localhost",
-  port: 1433,
-  username: "sa",
-  password: "IXT%)_1069",
-  database: "fabritech_challenge",
+  type: process.env.DATABASE_TYPE as any,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   synchronize: true,
   logging: false,
   entities: [Address, Client, Kinship, User],
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,13 @@
+import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: path.join(__dirname, '..', '.env')
+})
+
 import app from './app'
 
-app.listen(3000, () => {
+const port = process.env.PORT || 5000
+app.listen(port, () => {
   console.log('Server open in http://localhost:3000')
 })
