d1e5cd432f3fcd00404499e58ba4341155b3f2f7
renato3x <pereirarenato21@gmail.com>
2022-11-11 11:20:04 -0300

    frontend: signup from controll added

--- frontend/src/app/authentication/authentication.module.ts
+++ frontend/src/app/authentication/authentication.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { SignupComponent } from './pages/signup/signup.component';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { MaterialModule } from '../material/material.module';
   imports: [
     CommonModule,
     AuthenticationRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthenticationModule { }
--- frontend/src/app/authentication/pages/signup/signup.component.ts
+++ frontend/src/app/authentication/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup = this.builder.group({
+    firstName: ['', [ Validators.required ]],
+    lastName: ['', [ Validators.required ]],
+    username: ['', [ Validators.required, Validators.minLength(5) ]],
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(10) ]],
+    confirmPassword: ['', [ Validators.required, Validators.minLength(10) ]]
+  })
+
+  constructor(
+    private builder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
-
 }
