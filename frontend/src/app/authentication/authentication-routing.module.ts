2936c177aa55146715d7b8f76d05b522451884e2
renato3x <pereirarenato21@gmail.com>
2022-11-11 12:38:29 -0300

    frontend: create signin route

--- frontend/src/app/authentication/authentication-routing.module.ts
+++ frontend/src/app/authentication/authentication-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
   }
 ]
 
--- frontend/src/app/authentication/authentication.module.ts
+++ frontend/src/app/authentication/authentication.module.ts
@@ -5,11 +5,13 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { SignupComponent } from './pages/signup/signup.component';
 import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SigninComponent } from './pages/signin/signin.component';
 
 
 @NgModule({
   declarations: [
-    SignupComponent
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     CommonModule,
