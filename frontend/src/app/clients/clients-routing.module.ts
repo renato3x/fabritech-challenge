740487c4ae9e547e26b866d1ac17d1235622e96a
renato3x <pereirarenato21@gmail.com>
2022-11-13 14:31:11 -0300

    frontend: new client page created

--- frontend/src/app/clients/clients-routing.module.ts
+++ frontend/src/app/clients/clients-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TokenVerificationGuard } from '../globals/guards/token-verification.guard';
 import { ClientsListComponent } from './pages/clients-list/clients-list.component';
+import { NewClientComponent } from './pages/new-client/new-client.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ClientsListComponent,
+    canActivate: [
+      TokenVerificationGuard
+    ]
+  },
+  {
+    path: 'new',
+    component: NewClientComponent,
     canActivate: [
       TokenVerificationGuard
     ]
--- frontend/src/app/clients/clients.module.ts
+++ frontend/src/app/clients/clients.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientsListComponent } from './pages/clients-list/clients-list.component';
 import { MaterialModule } from '../material/material.module';
+import { NewClientComponent } from './pages/new-client/new-client.component';
 
 
 @NgModule({
   declarations: [
-    ClientsListComponent
+    ClientsListComponent,
+    NewClientComponent
   ],
   imports: [
     CommonModule,
